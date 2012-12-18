52315465d17408901ada81c66dccb5bcb10b9dcc
John Fritter <jefritter@gmail.com>
2012-12-18 13:30:14 -0800

    here is my commit message

---
+++ talker.py
@@ -0,0 +1,15 @@
+import socket
+
+UDP_IP = "192.168.1.177"
+UDP_PORT = 8888
+MESSAGE = "t"
+
+print "UDP target IP:", UDP_IP
+print "UDP target port:", UDP_PORT
+print "message:", MESSAGE
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+a = 1
+
+while a == 1:
+    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
