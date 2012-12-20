ae5c30fddac554dbbf6e656352a5a5d7fc471452
John Fritter <jefritter@gmail.com>
2012-12-20 12:52:17 -0800

    another update

--- acknowledger.cpp
+++ acknowledger.cpp
@@ -8,7 +8,6 @@
 byte mac[] = {  
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192, 168, 1, 177);
-int ledPin = 13;
 
 unsigned int localPort = 8888;      // local port to listen on
 
@@ -24,7 +23,9 @@ void setup() {
   // start the Ethernet and UDP:
   Ethernet.begin(mac,ip);
   Udp.begin(localPort);
-
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
 }
 
 void loop() {
@@ -39,23 +40,43 @@ void loop() {
     
       // read the packet into packetBufffer
       Udp.read(packetBuffer,UDP_TX_PACKET_MAX_SIZE);
-      char foo = packetBuffer[0];
-      pinMode(ledPin, OUTPUT);
+      char Command[4];
+      for(int nCom = 0; nCom < 4; nCom++)
+        {
+        Command[nCom] = packetBuffer[nCom];
+        }
             
-      if (foo == 't')
+      if (Command[0] == 't')
       {
-        digitalWrite(ledPin, HIGH);
+        digitalWrite(13, HIGH);
       }
+      else if (Command[0] == 'r')
+      {
+        digitalWrite(12, HIGH);
+      }
+      else if (Command[0] == 'e')
+      {
+        digitalWrite(11, HIGH);
+      }   
+      else if (Command[0] == 'w')
+      {
+        for (int i = 0; i < 256; i++)
+        {
+          analogWrite(11, i);
+          delay(100);
+        }
+      }      
       else
       {
-        digitalWrite(ledPin, LOW);
+        digitalWrite(13, LOW);
+        digitalWrite(12, LOW);
+        digitalWrite(11, LOW);
       }
 
       // send a reply, to the IP address and port that sent us the packet we received
       itoa(packetSize, Size, 10);
       Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
       Udp.write(ReplyBuffer);
-      Udp.write(foo);
       Udp.write(packetBuffer);
       Udp.write(Size);
       Udp.endPacket();
@@ -64,7 +85,7 @@ void loop() {
     else
     {
       Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-      Udp.write("invalid input, please try again");
+      Udp.write("NOPE");
       Udp.endPacket();
     }
   }
